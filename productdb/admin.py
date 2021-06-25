e11a7edfae3d875c5026b409b30513e4b8953878
Nicolas Krein Werle <nikokrein@gmail.com>
2021-06-25 20:07:52 -0300

    Models Cliente + Pedido + filters

--- productdb/admin.py
+++ productdb/admin.py
@@ -3,5 +3,19 @@ from productdb.models import *
 
 # Register your models here.
 
+
+class Clientes_view(admin.ModelAdmin):
+    list_display = ("nombre", "telefono", "direccion")
+    search_fields = ("nombre",)
+
+
+class Pedidos_view(admin.ModelAdmin):
+    list_display = ("fecha", "cliente", "estado")
+    list_filter = ("fecha", "estado")
+    date_hierarchy = "fecha"
+
+
 admin.site.register(Tipo)
+admin.site.register(Cliente, Clientes_view)
+admin.site.register(Pedido, Pedidos_view)
 admin.site.register(Producto)
--- productdb/models.py
+++ productdb/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
+from django.db.models.fields import DateField
 
 # Create your models here.
 
@@ -13,16 +14,35 @@ class Tipo(models.Model):
         return txt.format(self.codigo, self.nombre)
 
 
+class Cliente(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=20)
+    direccion = models.CharField(max_length=20)
+    telefono = models.CharField(max_length=20)
+    email = models.EmailField(blank=True, null=True)
+
+    def __str__(self):
+        txt = "{0} - Tel.: {1}"
+        return txt.format(self.nombre, self.telefono)
+
+
+class Pedido(models.Model):
+    nro = models.AutoField(primary_key=True)
+    fecha = DateField(auto_now=True)
+    cliente = models.ForeignKey(Cliente, null=False, blank=False, on_delete=CASCADE)
+    estado = models.BooleanField(default=False)
+
+    def __str__(self):
+        txt = "{0} - Estado: {1}"
+        return txt.format(self.cliente, self.estado)
+
+
 class Producto(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=20)
     tipo = models.ForeignKey(Tipo, null=False, blank=False, on_delete=CASCADE)
     stock = models.CharField(max_length=10)
     precio = models.CharField(max_length=5)
-
-    def mostrar_producto(self):
-        txt = "{0} - Stock: {1} - Precio: {2}"
-        return txt.format(self.nombre, self.stock, self.precio)
 
     def __str__(self):
         txt = "{0} - Stock: {1} - Precio: {2}"
